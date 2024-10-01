4cecca671a482c136acc641af7b80ca7c60cfc95
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-10-01 09:27:22 +0200

    style: btn form

--- src/app/components/template-form/template-form.component.ts
+++ src/app/components/template-form/template-form.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Message } from '../../models/message.models';
 
 @Component({
   selector: 'app-template-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.scss'
 })
 export class TemplateFormComponent {
 
+  message: Message = {
+    lastname: '',
+    firstname: '',
+    email: '',
+    content: ''
+  };
+  
+  onReset(): void {
+    console.log('Resetting form');
+    
+    this.message = {
+      lastname: '',
+      firstname: '',
+      email: '',
+      content: ''
+    };
+  }
+
+  onSubmit(): void {
+    console.log(this.message);
+  }
 }
